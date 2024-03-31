ab41a422792482859293f535eaf040c781056257
Jhonny <jhonnycgarcia@gmail.com>
2024-03-31 14:25:38 -0500

    Acordeones dinámicos Agregado de elementos desplegables en la página de tableros

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -20,6 +20,36 @@ export class BoardsComponent implements OnInit {
   public faUsers = faUsers;
   public faGear = faGrinHearts;
 
+  public items = [
+    {
+      label: 'Item 1',
+      items: [
+        { label: 'Sub Item 1.1' },
+        { label: 'Sub Item 1.2' },
+        { label: 'Sub Item 1.3' },
+        { label: 'Sub Item 1.4' }
+      ]
+    },
+    {
+      label: 'Item 2',
+      items: [
+        { label: 'Sub Item 2.1' },
+        { label: 'Sub Item 2.2' },
+        { label: 'Sub Item 2.3' },
+        { label: 'Sub Item 2.4' }
+      ]
+    },
+    {
+      label: 'Item 3',
+      items: [
+        { label: 'Sub Item 3.1' },
+        { label: 'Sub Item 3.2' },
+        { label: 'Sub Item 3.3' },
+        { label: 'Sub Item 3.4' }
+      ]
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
