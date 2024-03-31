5383755e101b3eb1927aaa0f15a566d0d8d0e37a
Jhonny <jhonnycgarcia@gmail.com>
2024-03-31 17:25:33 -0500

    Listas con virtual scrolling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { OverlayModule } from '@angular/cdk/overlay';
 import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { DialogModule } from '@angular/cdk/dialog';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,6 +37,7 @@ import { ScrollComponent } from './pages/scroll/scroll.component';
     DialogModule,
     FontAwesomeModule,
     HttpClientModule,
+    ScrollingModule,
     AppRoutingModule
   ],
   providers: [],
