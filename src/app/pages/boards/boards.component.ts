96478587dd36136bba41a2e17dadb83f677db5b4
Jhonny <jhonnycgarcia@gmail.com>
2024-03-31 00:02:04 -0500

    Creando acordeones Agregado módulo CdkAccordionModule y funcionalidad de menú desplegable en la página de tableros

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
-import { faBox, faWaveSquare, faClock } from '@fortawesome/free-solid-svg-icons'
+import { faBox, faWaveSquare, faClock, faAngleUp, faAngleDown, faHeart, faBorderAll, faUsers, faGrinHearts } from '@fortawesome/free-solid-svg-icons'
 
 
 @Component({
@@ -13,6 +13,12 @@ export class BoardsComponent implements OnInit {
   public faBox = faBox;
   public faWaveSquare = faWaveSquare;
   public faClock = faClock;
+  public faAngleUp = faAngleUp;
+  public faAngleDown = faAngleDown;
+  public faHeart = faHeart;
+  public faBorderAll = faBorderAll;
+  public faUsers = faUsers;
+  public faGear = faGrinHearts;
 
   constructor() { }
 
