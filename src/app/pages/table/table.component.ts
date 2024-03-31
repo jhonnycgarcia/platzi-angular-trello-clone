f6b0383c79e9d5e662186a13da482e53684ff5b1
Jhonny <jhonnycgarcia@gmail.com>
2024-03-31 17:41:55 -0500

    Tablas

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -3,12 +3,13 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
 
 @Component({
-  selector: 'app-scroll',
-  templateUrl: './scroll.component.html'
+  selector: 'app-table',
+  templateUrl: './table.component.html'
 })
-export class ScrollComponent implements OnInit {
+export class TableComponent implements OnInit {
 
   public products: Product[] = [];
+  public columns: string[] = ['id', 'title', 'price', 'cover'];
 
   constructor(
     private httpClient: HttpClient
@@ -17,6 +18,7 @@ export class ScrollComponent implements OnInit {
   ngOnInit(): void {
     this.httpClient.get<Product[]>('https://api.escuelajs.co/api/v1/products')
     .subscribe((data) => {
+      // console.log('data', data);
       this.products = data;
     });
   }
