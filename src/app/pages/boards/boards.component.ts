4511f0c976ff3e280310b25c3c046509f3e49871
Jhonny <jhonnycgarcia@gmail.com>
2024-03-30 23:48:34 -0500

    Iconos en Angular con Font Awesome Agregadas configuraciones de Tailwind CSS, FontAwesome y componentes de Angular

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { faTrello } from '@fortawesome/free-brands-svg-icons';
+import { faBox, faWaveSquare, faClock } from '@fortawesome/free-solid-svg-icons'
+
 
 @Component({
   selector: 'app-boards',
   templateUrl: './boards.component.html'
 })
 export class BoardsComponent implements OnInit {
+
+  public faTrello = faTrello;
+  public faBox = faBox;
+  public faWaveSquare = faWaveSquare;
+  public faClock = faClock;
 
   constructor() { }
 
