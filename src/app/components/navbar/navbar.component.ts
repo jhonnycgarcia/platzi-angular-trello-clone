999dac2683241269dc14c3b771861d12d6392ff5
Jhonny <jhonnycgarcia@gmail.com>
2024-03-30 23:25:42 -0500

    Múltiples Overlays Agregados elementos al menú desplegable y al cuerpo del navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   public isOpen = false;
+  public isOpenBody = false;
 
   constructor() { }
 
